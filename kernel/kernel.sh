f40ebf6ed4e036b7b9c420b64e8d6dfcf44b5ac7
vocky-design <sword1458364124@foxmail.com>
2022-01-17 06:44:14 -0800

    add "lib/kernel/print.S" put_char()

--- kernel/kernel.sh
+++ kernel/kernel.sh
@@ -1,3 +1,4 @@
-gcc -m32 -c -o main.o main.c
-ld -m elf_i386 main.o -Ttext 0xc0001500 -e main -o kernel.bin
+nasm -f elf32 -l ../lib/kernel/print.lst -o ../lib/kernel/print.o  ../lib/kernel/print.S
+gcc -m32 -c -I ../lib/kernel/  -o main.o main.c
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel.bin main.o ../lib/kernel/print.o 
 dd if=kernel.bin of=../bochs/hd60M.img bs=512 count=200 seek=9 conv=notrunc
